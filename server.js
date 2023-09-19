450912c5eb93f37d6da85d2e664ea9845585e2c9
Satya Prakash <96316675+Satya1824@users.noreply.github.com>
2023-09-19 18:37:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,11 +7,6 @@ import authRoutes from "./routes/authRoutes.js";
 import tweetRoutes from "./routes/tweetRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import colors from "colors";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 // port
 const PORT = process.env.PORT || 5080;
@@ -41,7 +36,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, "./twitter-clone/build")));
 
 // routes
 app.use("/api/auth", authRoutes);
@@ -51,11 +45,6 @@ app.use("/api/user", userRoutes);
 // Serve static files from the "uploads" directory
 app.use("/uploads", express.static("uploads"));
 app.use("/profiles", express.static("profiles"));
-
-//rest api
-app.use("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./twitter-clone/build/index.html"));
-});
 
 // listen run
 app.listen(PORT, () => {
